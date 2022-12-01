ab52c1484c520507f81960fe70e84901664cedd5
Collin Brake <collin.brake@ventrac.com>
2022-12-01 15:10:44 -0500

    defined some scope in README and added message and signal structs

---
+++ message.go
@@ -0,0 +1,13 @@
+package canparse
+
+import (
+	"log"
+)
+
+// Message represents a CAN bus frame that has an id and data, chich is split into signals.
+type Message struct {
+	Id uint32
+	Name string
+	Length int
+	Signals []Signal
+}
--- signal.go
+++ signal.go
@@ -1,6 +1,22 @@
 package canparse
 
+type ByteOrder string
+
+const (
+	LittleEndian ByteOrder = "LittleEndian"
+	BigEndian ByteOrder = "BigEndian"
+)
+
+// Signal represents a meaningful part of a CAN message.
 type Signal struct {
 	Name string
 	Start int
+	Length int
+	ByteOrder ByteOrder
+	IsSigned bool
+	Scale float64
+	Offset float64
+	Minimum float64
+	Maximum float64
+	Unit string
 }
