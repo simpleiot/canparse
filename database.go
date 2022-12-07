414e3f040b0c7da7dc57c9af764dac9c5c824ca5
Collin Brake <collin.brake@ventrac.com>
2022-12-07 14:10:51 -0500

    start implimenting dbc decoding

--- database.go
+++ database.go
@@ -2,12 +2,16 @@ package canparse
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 
 	"github.com/pkg/errors"
+	"go.einride.tech/can/pkg/dbc"
 )
 
 // Database represents a generic CAN database with multiple CAN busses.
@@ -15,35 +19,97 @@ type Database struct {
 	Busses []Bus
 }
 
-// ReadKcd erases any information in the database and populates the database
-// with the information parsed from the KCD file.
-func (db *Database) ReadKcd(filePathKcd string) error {
-
-	// Make sure the database is clean
+// Clean empties the database
+func (db *Database) Clean() {
 	db.Busses = []Bus{}
+}
 
-	kcdFile, err := os.Open(filePathKcd)
+// Read takes the path to a .dbc or .kcd CAN database file and parses it. It then
+// appends the information to the generic Database.
+func (db *Database) Read(filePathDb string) error {
+	file, err := os.Open(filePathDb)
 	if err != nil {
 		return errors.Wrap(err, "Canparse, error opening file at supplied path")
 	}
-	defer kcdFile.Close()
+	defer file.Close()
 
-	kcdData, err := ioutil.ReadAll(kcdFile)
+	bytes, err := ioutil.ReadAll(file)
 	if err != nil {
 		return errors.Wrap(err, "Canparse, error reading file contents")
 	}
 
+	err = db.ReadBytes(bytes, filePathDb)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ReadBytes takes bytes containing data in .dbc or .kcd format and parses it. It
+// then appends the information to the generic Database.
+func (db *Database) ReadBytes(bytes []byte, filePathDb string) error {
+	log.Println(filepath.Ext(filePathDb))
+	switch filepath.Ext(filePathDb) {
+	case ".dbc":
+		err := db.ReadDbc(bytes, filePathDb)
+		if err != nil {
+			return err
+		}
+	case ".kcd":
+		err := db.ReadKcd(bytes)
+		if err != nil {
+			return err
+		}
+	default:
+		return errors.New(fmt.Sprintf("canparse: invalid CAN database file extension: %v", filePathDb))
+	}
+	return nil
+}
+
+// ReadDbc takes bytes containing data in .dbc format and parses it. It
+// then appends the information to the generic Database.
+func (db *Database) ReadDbc(bytes []byte, filePathDb string) error {
+	parser := dbc.NewParser(filePathDb, bytes)
+	err := parser.Parse()
+	if err != nil {
+		return errors.Wrap(err, "Canparse, error parsing DBC data")
+	}
+	bus := &Bus{
+		Name: filePathDb,
+	}
+	defs := parser.Defs()
+	for _, d := range defs {
+		keyword := string(bytes[d.Position().Offset : d.Position().Offset+3])
+		log.Println(d.Position(), keyword)
+
+		// Code to detect dbc.MessageDef keyword and somehow parse the rest of the
+		// data as that???
+		if keyword != string(dbc.KeywordMessage) {
+			continue
+		}
+		//_ = d.Name
+
+		msg := Message{}
+		bus.Messages = append(bus.Messages, msg)
+	}
+
+	return nil
+}
+
+// ReadKcd takes bytes containing data in .kcd format and parses it. It
+// then appends the information to the generic Database.
+func (db *Database) ReadKcd(bytes []byte) error {
+
 	kcdDb := &KcdDatabase{}
-	err = xml.Unmarshal(kcdData, kcdDb)
+	err := xml.Unmarshal(bytes, kcdDb)
 	if err != nil {
 		return errors.Wrap(err, "Canparse, error parsing KCD data")
 	}
 
-	// Confert KCD database to generic database
-	for i, b := range kcdDb.KcdBusses {
-		db.Busses = append(db.Busses, Bus{})
+	// Append parsed KCD data to master Database
+	for _, b := range kcdDb.KcdBusses {
+		parsedMessages := []Message{}
 		for _, m := range b.KcdMessages {
-
 			// Copy over the message contents
 			var parsedMsg Message
 			parsedMsg.Name = m.Name
@@ -68,8 +134,9 @@ func (db *Database) ReadKcd(filePathKcd string) error {
 				parsedSig.Unit = s.KcdValue.Unit
 				parsedMsg.Signals = append(parsedMsg.Signals, parsedSig)
 			}
-			db.Busses[i].Messages = append(db.Busses[i].Messages, parsedMsg)
+			parsedMessages = append(parsedMessages, parsedMsg)
 		}
+		db.Busses = append(db.Busses, Bus{Name: b.Name, Messages: parsedMessages})
 	}
 
 	// Now the database should be populated with the data of the KCD file
